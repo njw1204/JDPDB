3768e1b770838201689e6d7e9005b5d404fc1932
Jongwoo Na <njw1204@naver.com>
2019-11-19 18:08:21 +0900

    Add signup func to backend

--- backend/dao/UserDAO.js
+++ backend/dao/UserDAO.js
@@ -5,20 +5,61 @@ const sqlHelper = require("./helper/sql-helper");
 class UserDAO {
     authUser(user, callback) {
         sqlHelper.query(
-            "SELECT id FROM animals_user WHERE email=? AND password=?",
+            "SELECT id FROM animals_user WHERE email=? AND password=PASSWORD(?)",
             [user.email, user.password],
             function(err, results, fields) {
-                if (err) callback(false);
-
-                if (results.length) {
-                    console.log("login success (id) : " + results[0].id);
-                    callback(true);
-                    return;
-                }
-                else {
+                if (err || results.length < 1) {
                     callback(false);
                     return;
                 }
+
+                console.log("login success (id) : " + results[0].id);
+                callback(true);
+            }
+        );
+    }
+
+    createUser(user, callback) {
+        sqlHelper.query(
+            "INSERT INTO animals_user(email,password,nickname,point) VALUES(?,PASSWORD(?),?,0)",
+            [user.email, user.password, user.nickname],
+            function(err, results, fields) {
+                if (err || results.affectedRows < 1) {
+                    callback(false);
+                    return;
+                }
+
+                callback(true);
+            }
+        );
+    }
+
+    checkExistedEmail(email, callback) {
+        sqlHelper.query(
+            "SELECT id FROM animals_user WHERE email=?",
+            [email],
+            function(err, results, fields) {
+                if (err || results.length < 1) {
+                    callback(false);
+                    return;
+                }
+
+                callback(true);
+            }
+        );
+    }
+
+    checkExistedNickname(nickname, callback) {
+        sqlHelper.query(
+            "SELECT id FROM animals_user WHERE nickname=?",
+            [nickname],
+            function(err, results, fields) {
+                if (err || results.length < 1) {
+                    callback(false);
+                    return;
+                }
+
+                callback(true);
             }
         );
     }
--- backend/router/main-router.js
+++ backend/router/main-router.js
@@ -37,5 +37,52 @@ router.get("/logout", function(req, res) {
     res.redirect("/login");
 });
 
+router.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
+router.post("/signup", function(req, res) {
+    let user = {
+        email: req.body.email,
+        password: req.body.password,
+        nickname: req.body.nickname,
+    };
+
+    if (!user.email || !user.password || !user.nickname || !req.body.password2) {
+        res.render("signup", {message: "모든 필드를 입력해주세요."});
+        return;
+    }
+
+    if (user.password !== req.body.password2) {
+        res.render("signup", {message: "비밀번호가 일치하지 않습니다."});
+        return;
+    }
+
+    let userDAO = new UserDAO();
+    userDAO.checkExistedEmail(user.email, function(result) {
+        if (result) {
+            res.render("signup", {message: "이미 가입된 이메일입니다."});
+            return;
+        }
+
+        userDAO.checkExistedNickname(user.nickname, function(result) {
+            if (result) {
+                res.render("signup", {message: "이미 존재하는 닉네임입니다."});
+                return;
+            }
+
+            userDAO.createUser(user, function(result) {
+                if (result) {
+                    req.session.user = user.email;
+                    res.redirect("/login");
+                }
+                else {
+                    res.render("signup", {message: "가입 실패"});
+                }
+            });
+        });
+    });
+});
+
 
 module.exports = router;
