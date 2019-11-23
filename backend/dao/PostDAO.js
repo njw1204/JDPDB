87384d60169b39ce2bbdcaabbe62daac20a33642
Jongwoo Na <njw1204@naver.com>
2019-11-23 14:55:25 +0900

    Connection not released bug fix in PostDAO

--- backend/dao/PostDAO.js
+++ backend/dao/PostDAO.js
@@ -19,40 +19,42 @@ class PostDAO {
                     `,
                     [id],
                     (err, results, fields) => {
-                        if (err) return reject(err);
+                        if (err) {
+                            conn.release();
+                            return reject(err);
+                        }
                         let calls = [];
 
                         for (let post of results[1]) {
                             calls.push(new Promise((resolve, reject) => {
-                                    conn.query(
-                                        `SELECT tag.id, tag.name
-                                         FROM animals_post_tags AS post_tags
-                                         INNER JOIN animals_tag AS tag ON post_tags.tag_id = tag.id
-                                         WHERE post_tags.post_id = ?;
+                                conn.query(
+                                    `SELECT tag.id, tag.name
+                                     FROM animals_post_tags AS post_tags
+                                     INNER JOIN animals_tag AS tag ON post_tags.tag_id = tag.id
+                                     WHERE post_tags.post_id = ?;
 
-                                         SELECT file.url
-                                         FROM animals_post_files AS post_files
-                                         INNER JOIN animals_file AS file ON post_files.file_id = file.id
-                                         WHERE post_files.post_id = ?;
+                                     SELECT file.url
+                                     FROM animals_post_files AS post_files
+                                     INNER JOIN animals_file AS file ON post_files.file_id = file.id
+                                     WHERE post_files.post_id = ?;
 
-                                         SELECT comment.id, user_id, nickname, content, created_time
-                                         FROM animals_comment AS comment
-                                         INNER JOIN animals_user AS user ON user.id = comment.user_id
-                                         WHERE comment.post_id = ?
-                                         ORDER BY comment.id DESC
-                                        `,
-                                        [post.id, post.id, post.id],
-                                        (err, results, fields) => {
-                                            if (err) return reject(err);
+                                     SELECT comment.id, user_id, nickname, content, created_time
+                                     FROM animals_comment AS comment
+                                     INNER JOIN animals_user AS user ON user.id = comment.user_id
+                                     WHERE comment.post_id = ?
+                                     ORDER BY comment.id DESC
+                                    `,
+                                    [post.id, post.id, post.id],
+                                    (err, results, fields) => {
+                                        if (err) return reject(err);
 
-                                            post.tags = results[0];
-                                            post.files = results[1];
-                                            post.comments = results[2];
-                                            resolve(post);
-                                        }
-                                    );
-                                })
-                            );
+                                        post.tags = results[0];
+                                        post.files = results[1];
+                                        post.comments = results[2];
+                                        resolve(post);
+                                    }
+                                );
+                            }));
                         }
 
                         Promise.all(calls).then((values) => {
@@ -65,6 +67,8 @@ class PostDAO {
                             });
                         }).catch((err) => {
                             reject(err);
+                        }).finally(() => {
+                            conn.release();
                         });
                     }
                 );
